43ec7480e7f184a6e200bbcd8b60a9a7dba8a00a
Daniel <daniel.macdonald@my.jcu.edu.au>
2019-04-04 11:08:50 +1000

    No Repeating numbers because of the while loop. Fixed Print Line with help from solutions

--- prac_01/prac_04/quick_picks.py
+++ prac_01/prac_04/quick_picks.py
@@ -1,9 +1,9 @@
-
 import random
 
 NUMBERS_PER_LINE = 6
-MINIMUM = 1
-MAXIMUM = 45
+MINIMUM_NUMBER = 1
+MAXIMUM_NUMBER = 45
+
 
 def main():
     number_of_lines = int(input("How many quick picks: "))
@@ -14,13 +14,16 @@ def main():
     for i in range(number_of_lines):
         quick_pick = []
         for j in range(NUMBERS_PER_LINE):
-            number = random.randint(MINIMUM, MAXIMUM)
-            if number in quick_pick:
-                number = random.randint(MINIMUM, MAXIMUM)
+            number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+            while number in quick_pick:
+                number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
 
             quick_pick.append(number)
             quick_pick.sort()
 
-        print(quick_pick)
+        print(" ".join("{:2}".format(number) for number in quick_pick))
+
 
 main()
+
+# print(" ".join("{:2}".format(number) for number in quick_pick))
