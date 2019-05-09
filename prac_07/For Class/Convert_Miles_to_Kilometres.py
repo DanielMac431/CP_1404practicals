84e74dbd16473012d2e7c028144e979216b5afe8
Daniel <daniel.macdonald@my.jcu.edu.au>
2019-05-10 00:02:16 +1000

    Converter Working

--- prac_07/For Class/Convert_Miles_to_Kilometres.py
+++ prac_07/For Class/Convert_Miles_to_Kilometres.py
@@ -1,17 +1,30 @@
 from kivy.app import App
 from kivy.lang import Builder
 
+CONVERSION_CONSTANT = 1.60934
 class DistanceConversion(App):
     def build(self):
         self.title = "Convert Miles to Kilometres"
-        self.root = Builder.load_file('Convert Miles to Kilometres.kv')
+        self.root = Builder.load_file('Convert Milest to Kilometres.kv')
         return self.root
 
-    def handle_up(self)
-        
+    def handle_increment(self, user_input, increment):
+        number = self.input_check(user_input) + increment
+        self.root.ids.input_number.text = str(number)
 
 
+    def handle_convert(self, value):
+        result = self.input_check(value) * CONVERSION_CONSTANT
+        self.root.ids.output_label.text = str(result)
 
+    def input_check(self, text):
+        try:
+            input_number = float(text)
+            return input_number
+        except ValueError:
+            return 0.0
+
+DistanceConversion().run()
     # class BoxLayoutDemo(App):
     #     def build(self):
     #         self.title = "Box Layout Demo"
